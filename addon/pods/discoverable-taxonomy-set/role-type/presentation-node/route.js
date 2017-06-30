fbe212ddaeda6cc5983508478b2c5e312d939390
Seosamh Cahill <seo.cahill@gmail.com>
2017-06-30 20:10:41 +0200

    make sure to get all node relationships

--- addon/pods/discoverable-taxonomy-set/role-type/presentation-node/route.js
+++ addon/pods/discoverable-taxonomy-set/role-type/presentation-node/route.js
@@ -3,7 +3,9 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   store: Ember.inject.service(),
   model(params) {
-    return this.get('store').findRecord('presentation-node', params.presentation_node_id)
+    return this.get('store').findRecord('presentation-node', params.presentation_node_id, {
+      include: 'element,element.dimension-nodes'
+    })
   },
 
   afterModel(model) {
