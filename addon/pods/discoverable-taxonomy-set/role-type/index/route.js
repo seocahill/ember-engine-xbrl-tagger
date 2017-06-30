49f5aac3d6d05d7d580439d1d43ac823a4657dad
Seosamh Cahill <seo.cahill@gmail.com>
2017-06-30 20:09:38 +0200

    go straight to nodes index

--- addon/pods/discoverable-taxonomy-set/role-type/index/route.js
+++ addon/pods/discoverable-taxonomy-set/role-type/index/route.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   afterModel() {
-    // this.replaceWith('discoverable-taxonomy-set.role-type.presentation-nodes');
+    this.replaceWith('discoverable-taxonomy-set.role-type.presentation-nodes');
   }
 });
