911f7693557e1b4d3c31fe707026d7ca1d554003
Seosamh Cahill <seo.cahill@gmail.com>
2017-06-30 20:09:00 +0200

    dont need root nodes here

--- addon/pods/discoverable-taxonomy-set/role-type/controller.js
+++ addon/pods/discoverable-taxonomy-set/role-type/controller.js
@@ -1,9 +1,3 @@
 import Ember from 'ember';
 
-const { computed } = Ember;
-
-export default Ember.Controller.extend({
-  rootNodes: computed.filter('model.presentationNodes', function(node) {
-    return node.get('parent.id') === undefined; 
-  })
-});
+export default Ember.Controller.extend({});
