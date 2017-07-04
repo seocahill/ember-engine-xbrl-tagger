fd7d83ccd2b64e82cc87de45ee86e1108ab9082f
Seosamh Cahill <seo.cahill@gmail.com>
2017-07-04 19:11:51 +0200

    add dimension action

---
+++ addon/pods/discoverable-taxonomy-set/role-type/presentation-node/element/dimension-node/controller.js
@@ -0,0 +1,23 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  currentCell: Ember.inject.service(),
+  store: Ember.inject.service(),
+
+  actions: {
+    addDimension(node) {
+      const cell = this.get('currentCell.cell');
+      const hypercubeDimension = this._hypercubeDimension(node);
+      const domainMember = node.get('name');
+      this.get('store').createRecord('dimension', { cell, hypercubeDimension, domainMember });
+    }
+  },
+
+  _hypercubeDimension(node) {
+    const parent = node.get('parent');
+    if (parent.get('shortArcrole') === 'hypercube-dimension') {
+      return parent.get('name');
+    }
+    return this._hypercubeDimension(parent);
+  }
+});
