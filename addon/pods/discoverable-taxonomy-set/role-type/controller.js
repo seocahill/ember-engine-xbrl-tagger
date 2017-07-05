20da1d1614b7064d248ce33f262a30e3ccce7c49
Seosamh Cahill <seo.cahill@gmail.com>
2017-07-05 13:17:45 +0200

    Save tagging info working

--- addon/pods/discoverable-taxonomy-set/role-type/controller.js
+++ addon/pods/discoverable-taxonomy-set/role-type/controller.js
@@ -1,5 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  currentCell: Ember.inject.service()
+  currentCell: Ember.inject.service(),
+
+  actions: {
+    updateCurrentCell() {
+      const cell = this.get('currentCell.cell');
+      cell.save().then(() => {
+        cell.get('dimensions').forEach((dimension) => dimension.save());
+      });
+    }
+  }
 });
