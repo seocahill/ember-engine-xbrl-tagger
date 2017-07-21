031fd2d32c3ee2d64b5b2f1521733e14171520ea
Seosamh Cahill <seo.cahill@gmail.com>
2017-07-21 15:53:42 +0200

    Start with hypercubes

--- addon/pods/discoverable-taxonomy-set/role-type/presentation-node/element/dimension-nodes/route.js
+++ addon/pods/discoverable-taxonomy-set/role-type/presentation-node/element/dimension-nodes/route.js
@@ -4,6 +4,6 @@ export default Ember.Route.extend({
   model() {
     return this.modelFor('discoverable-taxonomy-set.role-type.presentation-node.element')
       .get('dimensionNodes')
-      .filter((node) => node.get('arcrole') === "primary-item");
+      .filter((node) => node.get('arcrole') === "http://xbrl.org/int/dim/arcrole/all");
   }
 });
