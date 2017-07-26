54a32bbb589ebddb6885aeceb69e76f71804d382
Seosamh Cahill <seo.cahill@gmail.com>
2017-07-26 14:06:13 +0200

    Use first role as default if not set

--- addon/pods/discoverable-taxonomy-set/role-types/route.js
+++ addon/pods/discoverable-taxonomy-set/role-types/route.js
@@ -1,7 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  currentCell: Ember.inject.service(),
+  store: Ember.inject.service(),
+  currentRole: Ember.computed.alias('currentCell.roleType'),
+
   afterModel(model) {
     this.controllerFor('application').set('searchScope', model.get('roleTypes'));
+    if (! this.get('currentRole')) {
+      this.set('currentRole', this.get('store').peekAll('role-type').get('firstObject'));
+    }
   } 
 });
