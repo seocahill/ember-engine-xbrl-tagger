3add3203cc76a770263e8724c858e452d372b853
Seosamh Cahill <seo.cahill@gmail.com>
2017-07-28 14:17:31 +0200

    Fixed Dimensions UI

--- addon/pods/dimension-node/controller.js
+++ addon/pods/dimension-node/controller.js
@@ -3,13 +3,17 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   currentCell: Ember.inject.service(),
   store: Ember.inject.service(),
+  taxonomy: Ember.computed('currentCell.taxonomyId', function () {
+    return this.get('store').peekRecord('discoverable-taxonomy-set', this.get('currentCell.taxonomyId'));
+  }),
 
   actions: {
     async addDimension(node) {
       const cell = this.get('currentCell.cell');
       const hypercubeDimension = this._hypercubeDimension(node);
       const domainMember = node.get('tag');
-      await this.get('store').createRecord('dimension', { cell, hypercubeDimension, domainMember }).save();
+      const taxonomyName = this.get('taxonomy.name');
+      await this.get('store').createRecord('dimension', { cell, taxonomyName, hypercubeDimension, domainMember }).save();
       this.transitionToRoute('dimensions');
     }
   },
--- addon/pods/dimensions/route.js
+++ addon/pods/dimensions/route.js
@@ -2,5 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   store: Ember.inject.service(),
-  currentCell: Ember.inject.service()
+  currentCell: Ember.inject.service(),
+
+  model() {
+    return this.get('store').query('dimension', { 
+      filter: {
+        taxonomyName: "ie-gaap" 
+      }
+    });
+  }
 })
