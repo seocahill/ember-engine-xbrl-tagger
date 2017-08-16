43f0fb824e17c82c969fd6e725a6d5195d541d69
Seosamh Cahill <seo.cahill@gmail.com>
2017-08-16 13:48:36 +0200

    Tweak fixture data

--- tests/dummy/mirage/fixtures/discoverableTaxonomySets.js
+++ tests/dummy/mirage/fixtures/discoverableTaxonomySets.js
@@ -2,18 +2,11 @@ export default [
   {
     "id": "1",
     "name": "UK-GAAP",
-    roleTypleIds: [1, 2]
+    "roleTypeIds": ["1", "2"]
   },
   {
     "id": "2",
-    "name": "UK-IFRS",
-  },
-  {
-    "id": "3",
     "name": "IE-GAAP",
+    "roleTypeIds": ["1", "2"]
   },
-  {
-    "id": "4",
-    "name": "IE-IFRS",
-  }
 ];
