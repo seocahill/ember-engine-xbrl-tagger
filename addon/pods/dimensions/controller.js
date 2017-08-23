b6115243a82492290a1ca25325da11df7aaf0b0b
Seosamh Cahill <seo.cahill@gmail.com>
2017-08-23 15:28:55 +0200

    Remove unused action

--- addon/pods/dimensions/controller.js
+++ addon/pods/dimensions/controller.js
@@ -3,16 +3,8 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   currentCell: Ember.inject.service(),
   dimensions: Ember.computed.alias('currentCell.cell.dimensions'),
-  cellDimensionIds: Ember.computed.mapBy('dimensions', 'id'),
   
   actions: {
-
-    selectDimensions(event) {
-      const selected = Ember.$(event.target).val();
-      const dimensions = this.get('model').filter((dimension) => selected.includes(dimension.get('id')));
-      this.set('dimensions', dimensions);
-    },
-
     saveCell() {
       this.get('currentCell.cell').save();
     }
--- addon/pods/discoverable-taxonomy-set/role-type/controller.js
+++ addon/pods/discoverable-taxonomy-set/role-type/controller.js
@@ -1,14 +1,3 @@
 import Ember from 'ember';
 
-export default Ember.Controller.extend({
-  currentCell: Ember.inject.service(),
-
-  actions: {
-    updateCurrentCell() {
-      const cell = this.get('currentCell.cell');
-      cell.save().then(() => {
-        cell.get('dimensions').forEach((dimension) => dimension.save());
-      });
-    }
-  }
-});
+export default Ember.Controller.extend({});
