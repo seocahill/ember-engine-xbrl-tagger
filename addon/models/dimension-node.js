9a5710746e57f711a92f19223220aeaa775b41c1
Seosamh Cahill <seo.cahill@gmail.com>
2017-08-30 17:28:49 +0200

    Add default info to aid tagging

--- addon/models/dimension-node.js
+++ addon/models/dimension-node.js
@@ -6,10 +6,13 @@ export default DS.Model.extend({
   roleType: DS.belongsTo('role-type'),
   children: DS.hasMany('dimension-node', { inverse: 'parent' }),
   parent: DS.belongsTo('dimension-node', { inverse: 'children' }),
+
   order: DS.attr('number'),
   arcrole: DS.attr('string'),
   name: DS.attr('string'),
   tag: DS.attr('string'),
+  hasDefaults: DS.attr('boolean'),
+  defaultDimension: DS.attr(),
 
   shortArcrole: Ember.computed('arcrole', function() {
     return this.get('arcrole').split('/').pop();
